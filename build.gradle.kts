18e4e39dd9230b39953419176a8d84396665e86a
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-08-27 12:31:54 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "+"
+    id("org.openjfx.javafxplugin") version "+"
+    id("application")
+    id("idea")
+}
+
+group = "de.lisa.studiumsorganisationswebscraper"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:+"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.projectlombok:lombok:+")
+    implementation ("com.google.code.gson:gson:+")
+    annotationProcessor("org.projectlombok:lombok:+")
+    testCompileOnly("org.projectlombok:lombok:+")
+    testAnnotationProcessor("org.projectlombok:lombok:+")
+    implementation ("org.jsoup:jsoup:+")
+}
+
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
+
+javafx{
+    version = "17"
+    modules("javafx.controls", "javafx.fxml")
+}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+application {
+    mainClass.set("de.lisa.studiumsorganisationswebscraper:Main")
+}
